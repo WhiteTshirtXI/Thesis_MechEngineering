ed74ffab2189e65603f346073d2b93ce9d5be6b8
Henrique Mageste <henrique.mageste@gmail.com>
2017-10-12 17:24:58 -0300

    bug: Set geometry to pipe

--- exp3_32SpNoPD_Pin_NagHishPark.f
+++ exp3_32SpNoPD_Pin_NagHishPark.f
@@ -14,7 +14,7 @@ c      DATA NITERM,URF/1,14*0.3/		!initially URF=0.3
       DATA ICCOUNT/0/
 
 C-----GRID-RELATED INDECES
-      DATA INGEOM/1/			!1-channel, 2- pipe
+      DATA INGEOM/2/			!1-channel, 2- pipe
       DATA NY/99/
       DATA DELTA,XLAST,ANGLE,RI/5.0E-1,40.,0.,0./	!delta=pipe radius or channel half-height
       DATA DYIDX,DYEDX,DDELDX/3*0.0/
